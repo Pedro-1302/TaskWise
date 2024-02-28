ba038e565ed642d45e78fe0fe42754603b406cf9
Pedro Franco <mrcraftsoficcial@gmail.com>
2024-02-28 07:56:59 -0300

    Added logic for delete a row (not inside firebase, but in the app

--- uikit-to-do-list/Controller/TasksViewController.swift
+++ uikit-to-do-list/Controller/TasksViewController.swift
@@ -81,11 +81,33 @@ class TasksViewController: UIViewController {
     }
 }
 
+extension TasksViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.beginUpdates()
+            
+            tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.reloadData()
+
+            tableView.endUpdates()
+        }
+    }
+}
+
 extension TasksViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
     }
-    
+        
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as? TaskTableViewCell
 
