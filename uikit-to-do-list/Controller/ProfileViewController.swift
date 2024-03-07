522f131d1f3350e96107b9d9368ac116c2d89b61
Pedro Franco <mrcraftsoficcial@gmail.com>
2024-03-07 19:41:22 -0300

    Added logic for use photos from gallery of take a photo to put in profile screen (design in progress)

--- uikit-to-do-list/Controller/ProfileViewController.swift
+++ uikit-to-do-list/Controller/ProfileViewController.swift
@@ -9,7 +9,56 @@ import UIKit
 import FirebaseAuth
 
 class ProfileViewController: UIViewController {
-
+//    @IBOutlet weak var myImageView: UIImageView!
+//    
+//    
+//    @IBAction func chooseImageAction(_ sender: Any) {
+//        showImagePickerOptions()
+//    }
+    
+    func imagePicker(sourceType: UIImagePickerController.SourceType) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        return imagePicker
+    }
+    
+    func showImagePickerOptions() {
+        let alertVC = UIAlertController(title: "Pick a Photo", message: "Choose a picture from Librar or", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { [weak self] (action) in
+            guard let self = self else {
+                return
+            }
+            
+            let cameraImagePicker = self.imagePicker (sourceType: .camera)
+            cameraImagePicker.delegate = self
+            
+            self.present (cameraImagePicker, animated: true) {
+                //TODO
+            }
+        }
+        
+        let libraryAction = UIAlertAction(title: "Library", style: .default) { [weak self] (action) in
+            guard let self = self else {
+                return
+            }
+            
+            let libraryImagePicker = self.imagePicker(sourceType: .photoLibrary)
+            libraryImagePicker.delegate = self
+            
+            self.present (libraryImagePicker, animated: true) {
+                //TODO
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertVC.addAction(cameraAction)
+        alertVC.addAction(libraryAction)
+        alertVC.addAction(cancelAction)
+        
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -23,4 +72,12 @@ class ProfileViewController: UIViewController {
         }
     }
     
+}
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as! UIImage
+        // self.myImageView.image = image
+        self.dismiss(animated: true, completion: nil)
+    }
 }
